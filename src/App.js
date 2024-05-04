9b6479b9d00f0db22ff4172b6a6aaeaddeeee036
Ahmad Saad <64619851+saadpocalypse@users.noreply.github.com>
2024-05-04 17:01:58 +0500

    chore: Add @vercel/analytics package for page tracking

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { GameProvider } from "./context/GameContext";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
     return (
         <GameProvider>
             <Router>
+                <Analytics />
                 <Routes>
                     <Route path="/" element={<Home />} />
                 </Routes>
